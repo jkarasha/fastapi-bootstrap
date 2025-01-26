c6c2795f97b09a67b1c2d13c57de6dfbf66710ca
Joe Karasha <jkarasha@gmail.com>
2025-01-25 19:08:28 -0800

    Add first_name, last_name to model.

--- app/users/models.py
+++ app/users/models.py
@@ -1,11 +1,13 @@
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import AsyncSession 
+from sqlalchemy import Column, String
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi_users.db import SQLAlchemyBaseUserTableUUID, SQLAlchemyUserDatabase
 
 from ..core.database import Base, get_async_session
 
 class User(SQLAlchemyBaseUserTableUUID, Base):
-    pass
+    first_name: str = Column(String(length=128), nullable=True)
+    last_name: str = Column(String(length=128), nullable=True)
 
 async def get_user_db(session: AsyncSession = Depends(get_async_session)):
     yield SQLAlchemyUserDatabase(session, User)
